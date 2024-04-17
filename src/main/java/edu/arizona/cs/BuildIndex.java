9db33fbeadd1b8677a7694a379e4137c3fb1f8e2
junfeng1116 <junfeng1116@email.arizona.edu>
2024-04-16 23:26:51 -0700

    read wiki files

--- src/main/java/edu/arizona/cs/BuildIndex.java
+++ src/main/java/edu/arizona/cs/BuildIndex.java
@@ -1,5 +1,37 @@
 package edu.arizona.cs;
 
+import java.io.File;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
 public class BuildIndex {
+
+    public BuildIndex(){
+        
+    }
+
+    public void fileIndex(String inputFileDir) throws java.io.FileNotFoundException,java.io.IOException {
+        // create the index file
+        String indexFile = "index-file";
+        File indexF = new File(indexFile);
+        // using Lucene to index
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Directory index = FSDirectory.open(indexF.toPath());
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter w = new IndexWriter(index, config);
+
+        // loop through all the wiki files under the directory
+        File wikiDirF = new File(inputFileDir);
+        for(String i : wikiDirF.list()) {
+            String singleFilePath = inputFileDir + "/" + i;
+            System.out.println(singleFilePath);
+        }
+        w.close();
+        index.close();
+    }
     
 }
--- src/main/java/edu/arizona/cs/MainWatson.java
+++ src/main/java/edu/arizona/cs/MainWatson.java
@@ -4,11 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MainWatson {
-    static boolean buildIndex = false;
+    static boolean buildIndex = true;
 	static boolean runQuery = true;
+	static String wikiDir = "src/main/resources/wiki-folder";
     public static void main(String[] args ) throws FileNotFoundException, IOException {
 		if(buildIndex) {
-			
+			BuildIndex myBuildIndex = new BuildIndex();
+			myBuildIndex.fileIndex(wikiDir);
 		}
 		if(runQuery) {
 			System.out.println("hello watson");
